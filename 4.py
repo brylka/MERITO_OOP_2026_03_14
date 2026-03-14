fff9b1632cd1d14920b870b61885e5d937dbdcbc
Bartosz Bryniarski <brylka@jelenia.com>
2026-03-14 09:59:40 +0100

    Sobota

--- 4.py
+++ 4.py
@@ -7,32 +7,48 @@ class Car:
             self.speed += 10
         else:
             print(f"Limit prędkości {self.name} osiągnięty!")
-    def status(self):
+    def brake(self):
         if self.speed > 0:
+            self.speed -= 10
+    def status(self):
+        if self.speed != 0:
             print(f"Samochód {self.name} porusza się z prędkością {self.speed}km/h")
         else:
             print(f"Samochód {self.name} stoi.")
     def honk(self):
         print(f"Samochód {self.name} trąbi")
 
-fiat = Car("Fiat")
 bmw = Car("BMW")
-audi = Car("Audi")
-audi.status()
+# bmw.speed = -100
+bmw.status()
+bmw.accelerate()
+bmw.status()
+bmw.brake()
+bmw.status()
+bmw.brake()
+bmw.status()
+bmw.accelerate()
+bmw.status()
+
+
+
 # bmw.speed = 200
-bmw.honk()
-for _ in range(20):
-    bmw.accelerate()
-    bmw.status()
+# bmw.honk()
+# for _ in range(20):
+#     bmw.accelerate()
+#     bmw.status()
 
 
 
 # bmw.status()
 #
 
-
-audi.accelerate()
-audi.status()
+# fiat = Car("Fiat")
+# audi = Car("Audi")
+# audi.status()
+#
+# audi.accelerate()
+# audi.status()
 # bmw.honk()
 # fiat.honk()
 # audi.status()
