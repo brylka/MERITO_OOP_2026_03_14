92a107f7a41b95cb410dcb3f749649d2f1ee98e0
Bartosz Bryniarski <brylka@jelenia.com>
2026-03-14 10:17:30 +0100

    Sobota

--- 4.py
+++ 4.py
@@ -2,14 +2,26 @@ class Car:
     def __init__(self, name):
         self.name = name
         self.speed = 0
+        self.alive = True
     def accelerate(self):
-        if self.speed < 120:
+        if not self.alive:
+            print(f"Samochód {self.name} jest popsuty!")
+        elif self.alive and self.speed < 120:
             self.speed += 10
         else:
             print(f"Limit prędkości {self.name} osiągnięty!")
     def brake(self):
         if self.speed > 0:
             self.speed -= 10
+    def crash(self, other_car):
+        if self.speed > 0:
+            print(f"Samochód {self.name} zderza się z samochodem {other_car.name}")
+            self.alive = False
+            other_car.alive = False
+            self.speed = 0
+            other_car.speed = 0
+        else:
+            print(f"Samochód {self.name} się nie porusza i nie może się zderzać!")
     def status(self):
         if self.speed != 0:
             print(f"Samochód {self.name} porusza się z prędkością {self.speed}km/h")
@@ -19,16 +31,28 @@ class Car:
         print(f"Samochód {self.name} trąbi")
 
 bmw = Car("BMW")
+audi = Car("Audi")
+bmw.accelerate()
+bmw.status()
+audi.status()
+bmw.crash(audi)
+bmw.accelerate()
+bmw.status()
+audi.accelerate()
+audi.status()
+
+
+
 # bmw.speed = -100
-bmw.status()
-bmw.accelerate()
-bmw.status()
-bmw.brake()
-bmw.status()
-bmw.brake()
-bmw.status()
-bmw.accelerate()
-bmw.status()
+# bmw.status()
+# bmw.accelerate()
+# bmw.status()
+# bmw.brake()
+# bmw.status()
+# bmw.brake()
+# bmw.status()
+# bmw.accelerate()
+# bmw.status()
 
 
 
