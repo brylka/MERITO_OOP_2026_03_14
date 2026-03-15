21efa07dfbe3129f5bd8fbd68051aad3dc904737
Bartosz Bryniarski <brylka@jelenia.com>
2026-03-15 09:17:08 +0100

    Niedziela

--- 6.py
+++ 6.py
@@ -7,9 +7,24 @@ class Animal:
 class Dog(Animal):
     def speak(self):
         print(f"{self.name} mówi: HAU HAU!")
+    def fetch(self):
+        print(f"{self.name} aportuje piłkę!")
+
+class Cat(Animal):
+    def speak(self):
+        print(f"{self.name} mówi: MIAU MIAU!")
+    def purr(self):
+        print(f"{self.name} mruczy...")
 
 if __name__ == '__main__':
     animal = Animal("Zwierzak")
     animal.speak()
     dog = Dog("Burek")
     dog.speak()
+    dog.fetch()
+    cat = Cat("Mruczek")
+    cat.speak()
+    cat.purr()
+
+# dodać kolejną klasę, która dziedziczy czy Animal lub innej klasie
+# potestować czy dziedziczą się metody z danej klasy
