a3528f5f26942b99f17e2138792a935b37cca7fb
Bartosz Bryniarski <brylka@jelenia.com>
2026-03-15 10:30:26 +0100

    Niedziela

--- 8.py
+++ 8.py
@@ -1,16 +1,16 @@
 class Dog:
     def __init__(self, name, age):
         self.name = name
-        self.age = age
+        self._age = age
     def get_age(self):
-        return self.age
+        return self._age
     def set_age(self, new_age):
         if new_age > 0:
-            self.age = new_age
+            self._age = new_age
 
 dog = Dog("Burek", 5)
 print(dog.name)
-print(dog.age)
+#print(dog.age)
 dog.set_age(8)
 print(dog.get_age())
 dog.age = -10
