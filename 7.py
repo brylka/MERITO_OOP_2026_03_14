ffb6f695e9f2e32e729920518d505d22ad0797e6
Bartosz Bryniarski <brylka@jelenia.com>
2026-03-15 10:03:32 +0100

    Niedziela

--- 7.py
+++ 7.py
@@ -1,17 +1,24 @@
 class Animal:
     def __init__(self, name):
         self.name = name
+        self.energy = 100
     def eat(self):
-        print(f"{self.name} je z miski...")
+        self.energy += 10
+        print(f"{self.name} je z miski...  Jego energia wynosi {self.energy}")
 
 class Dog(Animal):
+    def __init__(self, name, breed):
+        super().__init__(name)
+        # self.name = name
+        # self.energy = 100
+        self.breed = breed
     def eat(self):
         super().eat()
-        print(f"{self.name} merda ogonem z radości!")
+        print(f"{self.breed} o imieniu {self.name} merda ogonem z radości!")
 
 
 if __name__ == '__main__':
     animal = Animal("Zwierzak")
     animal.eat()
-    dog = Dog("Burek")
+    dog = Dog("Burek", "Kundel")
     dog.eat()
